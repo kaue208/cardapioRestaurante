16ec3e08694ca3d4edd23aed82ae49859dbfb2e9
Kau <ks5356014@gmail.com>
2026-04-24 23:27:33 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const modal = document.getElementById("myDialog");
+const modal2 = document.getElementById("myDialog2");
+const modal3 = document.getElementById("myDialog3");
+const modal4 = document.getElementById("myDialog4");
+const modal5 = document.getElementById("myDialog5");
+const modal6 = document.getElementById("myDialog6");
+const modal7 = document.getElementById("myDialog7");
+const abrirModal4 = document.getElementById("cardapio-item4");
+const fecharModal4 = document.getElementById("fechar4");
+const abrirModal5 = document.getElementById("cardapio-item5");
+const fecharModal5 = document.getElementById("fechar5");
+const abrirModal6 = document.getElementById("cardapio-item6");
+const fecharModal6 = document.getElementById("fechar6");
+const abrirModal7 = document.getElementById("cardapio-item7");
+const fecharModal7 = document.getElementById("fechar7");
+const abrirModal3 = document.getElementById("cardapio-item3");
+const fecharModal3 = document.getElementById("fechar3");
+const abrirModal2 = document.getElementById("cardapio-item2");
+const abrirModal = document.getElementById("cardapio-item");
+const fecharModal = document.getElementById("fechar");
+const fecharModal2 = document.getElementById("fechar2");
+
+abrirModal.addEventListener("click", function() {
+    modal.showModal();
+});
+
+fecharModal.addEventListener("click", () => {
+    modal.close();
+});
+
+abrirModal2.addEventListener("click", function() {
+        modal2.showModal();
+});
+
+fecharModal2.addEventListener("click", () => {
+    modal2.close();
+});
+
+abrirModal3.addEventListener("click", function() {
+    modal3.showModal();
+});
+
+fecharModal3.addEventListener("click", () => {
+    modal3.close();
+});
+
+abrirModal4.addEventListener("click", function() {
+    modal4.showModal();
+});
+
+fecharModal4.addEventListener("click", () => {
+    modal4.close();
+});
+
+abrirModal5.addEventListener("click", function() {
+    modal5.showModal();
+});
+
+fecharModal5.addEventListener("click", () => {
+    modal5.close();
+});
+
+abrirModal6.addEventListener("click", function() {
+    modal6.showModal();
+});
+
+fecharModal6.addEventListener("click", () => {
+    modal6.close();
+});
+
+abrirModal7.addEventListener("click", function() {
+    modal7.showModal();
+});
+
+fecharModal7.addEventListener("click", () => {
+    modal7.close();
+});
